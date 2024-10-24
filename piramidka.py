3d5f2a78227e4cdcf4b4f9ef28c96d2687f8218f
Kristina <tinka200507@mail.ru>
2024-10-24 20:50:36 +0400

    добавила текст

--- piramidka.py
+++ piramidka.py
@@ -3,3 +3,9 @@ def change_red():
     color_code_entry.delete(0, END)
     color_code_entry.insert(0, "#ff0000")
     color_label.config(text="Красный")
+    
+    color_label = Label(root, text="", font=("Arial", 12))
+
+
+color_code_entry = Entry(root, justify=CENTER)
+color_code_entry.pack()
